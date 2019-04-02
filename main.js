4d1d999ad8b3acd9c7996d7af0c575c58749ed32
Aligator_sah <saixiongh@outlook.com>
2019-04-02 09:15:05 -0400

    Added save item function

--- main.js
+++ main.js
@@ -28,4 +28,31 @@ document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
 function saveIssue(e) {
     var issueId = chance.guid();
     var issueDesc = document.getElementById('issueDescInput').value;
+    var issueSeverity = document.getElementById("issueSeverityInput").value;
+    var issueAssignedTo = document.getElementById("issueAssignedToInput").value;
+    var issueStatus = 'Open';
+
+    var issue = {
+        id: issueId,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+
+    if (localStorage.getItem('issues')  == null) {
+        var issues = [];
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    } else {
+        var issues = JSON.parse(localStorage.getItem('issues'));
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    }
+
+    document.getElementById('issueInputForm').reset();
+
+    fetchIssues();
+
+    e.preventDefault();
 }
