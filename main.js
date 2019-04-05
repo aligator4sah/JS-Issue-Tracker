21ec924260539b002ae701c9e3e934818df5c852
Aligator_sah <saixiongh@outlook.com>
2019-04-05 08:27:02 -0400

    Added close and delete function

--- main.js
+++ main.js
@@ -56,3 +56,32 @@ function saveIssue(e) {
 
     e.preventDefault();
 }
+
+function setStatusCloses(id) {
+    var issue = JSON.parse(localStorage.getItem('issues'));
+
+    for (var i = 0; i < issues.length; i++) {
+        if (issues[i].id == id) {
+         issues[i].status = "Closed";
+        }
+    }
+
+    localStorage.setItem('issues', JSON.stringify(issues));
+
+    fetchIssues();
+}
+
+
+function deleteIssue (id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+
+    for (var i = 0; i < issues.length; i++) {
+        if (issues[i].id === id) {
+            issues.splice(i, 1);
+        }
+    }
+
+    localStorage.setItem('issues', JSON.stringify(issues));
+
+    fetchIssues();
+}
